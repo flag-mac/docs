d119cff10ac6d33521950610c417f5e81d95506b
flag-mac <forget_98@qq.com>
2021-02-19 00:33:54 +0800

    修改

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -3,40 +3,52 @@ const moment = require('moment');
 moment.locale('zh-cn');
 
 module.exports = {
-  
-    title: '乔治 API 接口文档',
-    description: '乔治 API 接口文档，稳定免费',
-    head: [
-      ['link', { rel: 'icon', href: '/assets/img/favicon_32.ico' }],
-      ['link', { rel: 'icon', href: '/assets/img/favicon_48.ico' }],
-      ['link', { rel: 'icon', href: '/assets/img/favicon_64.ico' }],
-      ['link', { rel: 'icon', href: '/assets/img/favicon_128.ico' }],
-      ['meta',{ name: 'author',content: '乔治 API 接口文档' }],
-      ['meta',{ name: 'author',content: '乔治 API 接口文档，随机一言，随机动漫' }]
-    ],
-    markdown: {
-      lineNumbers: true
-    },
-    plugins: [
-      [
-        '@vuepress/last-updated',
-        {
-          transformer: (timestamp, lang) => {
-            return moment(timestamp).format('LLLL')
-          }
+
+  title: '乔治 API 接口文档',
+  description: '乔治 API 接口文档，稳定免费',
+  head: [
+    ['link', { rel: 'icon', href: '/assets/img/favicon_32.ico' }],
+    ['link', { rel: 'icon', href: '/assets/img/favicon_48.ico' }],
+    ['link', { rel: 'icon', href: '/assets/img/favicon_64.ico' }],
+    ['link', { rel: 'icon', href: '/assets/img/favicon_128.ico' }],
+    ['meta', { name: 'author', content: '乔治 API 接口文档' }],
+    ['meta', { name: 'author', content: '乔治 API 接口文档，随机一言，随机动漫' }]
+  ],
+  markdown: {
+    lineNumbers: true
+  },
+  plugins: [
+    [
+      '@vuepress/last-updated',
+      {
+        transformer: (timestamp, lang) => {
+          return moment(timestamp).format('LLLL')
         }
-      ]
-    ],  
-    themeConfig: {
-      nav: [
-        { text: '首页', link: '/' },
-        { text: '白名单', link: 'https://www.flagg.cn/examine.html' },
-      ],
-      sidebar: [
-        '/api/v1',
-        '/api/wallpaper'
-      ],
-      smoothScroll: true,
-      lastUpdated: '更新时间',
-    }
+      }
+    ],
+    [
+      '@vuepress/pwa', {
+        serviceWorker: true,
+        updatePopup: {
+          message: "有新的风暴出现",
+          buttonText: "刷新"
+        }
+      }
+    ],
+    [
+      '@vuepress/back-to-top'
+    ]
+  ],
+  themeConfig: {
+    nav: [
+      { text: '首页', link: '/' },
+      { text: '白名单', link: 'https://www.flagg.cn/examine.html' },
+    ],
+    sidebar: [
+      '/api/v1',
+      '/api/wallpaper'
+    ],
+    smoothScroll: true,
+    lastUpdated: '更新时间',
   }
+}
