b832b40f434bfe2df1cebf6eefe7c23874551996
flag-mac <forget_98@qq.com>
2021-02-18 23:54:54 +0800

    修改

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -6,6 +6,14 @@ module.exports = {
   
     title: '乔治 API 接口文档',
     description: '乔治 API 接口文档，稳定免费',
+    head: [
+      ['link', { rel: 'icon', href: '/assets/img/favicon_32.ico' }],
+      ['link', { rel: 'icon', href: '/assets/img/favicon_48.ico' }],
+      ['link', { rel: 'icon', href: '/assets/img/favicon_64.ico' }],
+      ['link', { rel: 'icon', href: '/assets/img/favicon_128.ico' }],
+      ['meta',{ name: 'author',content: '乔治 API 接口文档' }],
+      ['meta',{ name: 'author',content: '乔治 API 接口文档，随机一言，随机动漫' }]
+    ],
     markdown: {
       lineNumbers: true
     },
