f1ea69b49ba9051b1a871368d56840398e84ffce
flag-mac <forget_98@qq.com>
2021-02-19 01:14:20 +0800

    修改

--- docs/.vuepress/config/plugsConf.js
+++ docs/.vuepress/config/plugsConf.js
@@ -1,10 +1,11 @@
 const moment = require('moment');
+const secret = require('./secret');
 
 moment.locale('zh-cn');
 
 module.exports = {
     '@vuepress/last-updated' : {
-      transformer: (timestamp, lang) => moment(timestamp).format('LLLL')
+      secrit
     },
     '@vuepress/back-to-top' : true
   }
